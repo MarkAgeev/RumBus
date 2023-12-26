a47e43ac21187d57ce44b83672250314d9e5755c
Mark Ageev <markelloageev@gmail.com>
2023-12-26 17:47:04 +0200

    About block js

--- js/script.js
+++ js/script.js
@@ -165,9 +165,9 @@ function myFunction(element, num) {
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
+        // rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 200 &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
